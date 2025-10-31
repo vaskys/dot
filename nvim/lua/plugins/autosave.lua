81aa7f62aab35963a7659f430582003873e59001
Samuel Vasky <cotiajviemmladyxD@eksdi.com>
2025-10-31 22:57:36 +0100

    xd

--- nvim/lua/plugins/autosave.lua
+++
@@ -1,3 +0,0 @@
-return {
-	"https://github.com/pocco81/auto-save.nvim",
-}
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -8,78 +8,13 @@ return {
         "williamboman/mason-lspconfig.nvim",
     },
     config = function()
-        local nvim_lsp = require("lspconfig")
         local mason_lspconfig = require("mason-lspconfig")
-
-        local protocol = require("vim.lsp.protocol")
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-        mason_lspconfig.setup({
-            function(server)
-                nvim_lsp[server].setup({
-                    capabilities = capabilities,
-                })
-            end,
-            ["cssls"] = function()
-                nvim_lsp["cssls"].setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end,
-            ["tailwindcss"] = function()
-                nvim_lsp["tailwindcss"].setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end,
-            ["html"] = function()
-                nvim_lsp["html"].setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end,
-            ["jsonls"] = function()
-                nvim_lsp["jsonls"].setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end,
-            ["eslint"] = function()
-                nvim_lsp["eslint"].setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end,
-            ["pyright"] = function()
-                nvim_lsp["pyright"].setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end,
-            ["clangd"] = function()
-                nvim_lsp["clangd"].setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
-            end,
-           -- -- configure Swift server
-           ["sourcekit"] = function()
-            nvim_lsp["sourcekit"].setup({
-                capabilities = capabilities,
-                filetypes = { "swift" },
-            })
-           end,
-        })
-
-        nvim_lsp.sourcekit.setup({
-          -- capabilities = capabilities,
-          capabilities = {
-            workspace = {
-              didChangeWatchedFiles = {
-                dynamicRegistration = true,
-              },
-            },
-          },
+        vim.lsp.enable({
+              "clangd",
+              "sourcekit",
+              "pyright",
+              "stylua"
         })
     end,
 }
